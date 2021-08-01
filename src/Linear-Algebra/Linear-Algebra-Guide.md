a232b04df0178d971367aaee40cc6dd727639db1
Connor Keenum <Nellakprime@gmail.com>
2021-08-01 21:13:16 +0300

    Added Guide to Linear Algebra and Made a Basic Matrix Class with some functionality.

---
+++ src/Linear-Algebra/Linear-Algebra-Guide.md
@@ -0,0 +1,116 @@
+# Linear Algebra Guide
+
+## Table of Contents
+
+- [Definitions](#Definitions)
+- [Objects](#Objects)
+- [Equations](#Equations)
+- [Operations](#Operations)
+- [Properties](#Properties)
+- [Methods](#Methods)
+- [Proofs](#Proofs)
+
+## Definitions
+
+### Linear
+
+*Arranged in or extending along a straight or nearly straight line.*
+
+### Slope
+*Denotes how steep a line is an is canoically represented with the letter m in equations.*
+
+### Intercept
+*The point at which 2 lines intersect.*
+
+### Point-Slope Form of Linear Equation
+
+*Equation of a line in canonical form.*\
+<img src="http://www.sciweavers.org/tex2img.php?eq=y%20%3D%20mx%20%2B%20b&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0" align="center" border="0" alt="y = mx + b" width="96" height="19" />
+
+### Two-point from of Linear Equation
+
+*Equation of a line in form where you can find equation of line from 2 points.*\
+<img src="http://www.sciweavers.org/tex2img.php?eq=y%20-%20y%20_0%20%3D%20m%28x%20-%20x_0%29%2C%20m%20%3D%20%5Cfrac%7By_1%20-%20y_0%7D%7Bx_1%20-%20x_0%7D&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0" align="center" border="0" alt="y - y _0 = m(x - x_0), m = \frac{y_1 - y_0}{x_1 - x_0}" width="262" height="43" />
+
+### Scalar
+
+*A quantity having only magnitude, not direction.*\
+<img src="http://www.sciweavers.org/tex2img.php?eq=n&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0" align="center" border="0" alt="n" width="15" height="12" />
+
+### Vector
+
+*A quantity having direction as well as magnitude, especially as determining the position of one point in space relative to another.*\
+<img src="http://www.sciweavers.org/tex2img.php?eq=%20%5Cbegin%7Bbmatrix%7Da%20%5C%5Cb%20%5Cend%7Bbmatrix%7D%20&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0" align="center" border="0" alt=" \begin{bmatrix}a \\b \end{bmatrix} " width="31" height="39" />
+
+### Matrix
+
+*A rectangular array of quantities or expressions in rows and columns that is treated as a single entity and manipulated according to particular rules.*\
+<img src="http://www.sciweavers.org/tex2img.php?eq=%20%5Cbegin%7Bbmatrix%7Da%20%26%20b%20%5C%5Cc%20%26%20d%20%5Cend%7Bbmatrix%7D%20&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0" align="center" border="0" alt=" \begin{bmatrix}a & b \\c & d \end{bmatrix} " width="56" height="39" />
+
+### Vector Space
+
+*A space consisting of vectors, together with the associative and commutative operation of addition of vectors, and the associative and distributive operation of multiplication of vectors by scalars.*
+
+### Linear Equation
+
+*An equation between two variables that gives a straight line when plotted on a graph.*\
+<img src="http://www.sciweavers.org/tex2img.php?eq=%20%5Csum_%7Bj%3D1%7D%5En%20%7Ba_jx_j%7D%3Db%20&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0" align="center" border="0" alt=" \sum_{j=1}^n {a_jx_j}=b " width="94" height="53" />
+
+### System of Linear Equations
+
+*In mathematics, a system of linear equations (or linear system) is a collection of one or more linear equations involving the same set of variables.*\
+*Formal Definition:*\
+<img src="http://www.sciweavers.org/tex2img.php?eq=%20%5Csum_%7Bj%3D1%7D%5E%7Bn%7D%20%7Ba_i_j%20x_j%20%3D%20b_i%7D%2C%5C%5C%7B1%20%20%5Cleq%20i%20%20%5Cleq%20m%7D%20&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0" align="center" border="0" alt=" \sum_{j=1}^{n} {a_i_j x_j = b_i},\\{1  \leq i  \leq m} " width="110" height="74" />
+*For Example:*\
+<img src="http://www.sciweavers.org/tex2img.php?eq=2x%20%2B%202y%20-%20z%20%3D%201%5C%5C%0A2x%20-%202y%20%2B%204z%20%3D%20-2%5C%5C%0A-x%20%2B%201%2F2y%20-%20z%20%3D0%20&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0" align="center" border="0" alt="2x + 2y - z = 1\\2x - 2y + 4z = -2\\-x + 1/2y - z =0 " width="153" height="68" />
+
+### Solutions to Linear Equations
+
+*A System of Linear Equations can have 3 possible solutions:*
+-Unique Solution
+-No Solution
+-Infinitely Many Solutions
+
+### Reduced Row Echelon Form
+
+*The first non-zero number in the first row (the leading entry) is the number 1. The second row also starts with the number 1, which is further to the right than the leading entry in the first row. For every subsequent row, the number 1 must be further to the right.*
+
+*For Example:*\
+<img src="http://www.sciweavers.org/tex2img.php?eq=%20%5Cbegin%7Bbmatrix%7D1%20%26%200%20%5C%5C0%20%26%201%20%5Cend%7Bbmatrix%7D%20&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0" align="center" border="0" alt=" \begin{bmatrix}1 & 0 \\0 & 1 \end{bmatrix} " width="56" height="39" />
+
+## Objects
+
+- Vector
+- Matrix
+- Vector Space
+
+## Equations
+
+- Linear Equation
+- System of Linear Equations
+
+## Operations
+
+### Elementary Operators
+*These operators are your standard operators taught in elementary school.*\
+<img src="http://www.sciweavers.org/tex2img.php?eq=%20%2B%20%5C%5C%0A%20-%20%5C%5C%0A%20%5Ctimes%20%5C%5C%0A%20%5Cdiv%20%5C%5C%0A%3D&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0" align="center" border="0" alt=" + \\ - \\ \times \\ \div \\=" width="17" height="107" />
+
+
+### Summation Operator
+*This operator sums up all the values given by the expression in the range of var=num to limit.*\
+<img src="http://www.sciweavers.org/tex2img.php?eq=%20%5Csum_%7Bvar%3Dnum%7D%5E%7Blimit%7D%20%7Bexpression%7D&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0" align="center" border="0" alt=" \sum_{var=num}^{limit} {expression}" width="165" height="50" />
+
+## Properties
+**Associativity**
+*In mathematics, the associative property is a property of some binary operations, which means that rearranging the parentheses in an expression will not change the result.*\
+<img src="http://www.sciweavers.org/tex2img.php?eq=%28a%20%2B%20b%29%20%2B%20c%20%3D%20a%20%2B%20%28b%20%2B%20c%29&bc=White&fc=Black&im=jpg&fs=12&ff=arev&edit=0" align="center" border="0" alt="(a + b) + c = a + (b + c)" width="196" height="18" />
+
+*Applies to:*
+- Addition
+- Multiplication
+
+## Methods
+**Gaussian elimination**
+*Gaussian elimination, also known as row reduction, is an algorithm in linear algebra for solving a system of linear equations. It is usually understood as a sequence of operations performed on the corresponding matrix of coefficients.*
+
+## Proofs
---
+++ src/Linear-Algebra/Objects/Matrix.py
@@ -0,0 +1,357 @@
+'''A rectangular array of quantities or expressions in rows and columns that is treated as a single entity and manipulated according to particular rules.'''
+'''
+Representation: List of Lists
+Properties: rows, cols, A
+Methods: 
+    row Swap - Elementary Row Operation
+    row Multiply - Elementary Row operation
+    row Sum - Elementary Row operation
+    transpose
+    gaussian elimination
+    +, -, *, /, @ , where '@' is the Matrix Multiplication operator
+    to string
+'''
+
+class Matrix:
+
+    '''Matrix is initialized with a 2-dimensional array as input'''
+    def __init__(self, array):
+        self.rows = len(array)
+        self.cols = len(array[0])
+        self.A = array
+
+    '''Row Swap elementary row operation, Swap any two rows'''
+    def swap(self, row1, row2):
+        self.A[row1], self.A[row2] = self.A[row2], self.A[row1]
+        return self        
+
+    '''Scalar Multiplication elementary row operation, Multiply any row by a constant'''
+    def row_multiply(self,row1,scalar):
+        temp = Matrix([self.A[row1]])*scalar
+        self.A[row1] = temp.A[0]
+        return self
+
+    '''Row Sum elementary row operation, Add a multiple of one row to another row'''
+    '''Will add Row1 to Row2, and the result will be stored in Row2'''
+    def row_sum(self, row1, row2, multiple1=1, multiple2=1):
+        mul1 = multiple1*Matrix([self.A[row1]])
+        mul2 = multiple2*Matrix([self.A[row2]])
+        temp = mul1+mul2
+        self.A[row2] = temp.A[0]
+        return self
+
+    '''Matrix Transpose, switch Rows with Columns'''
+    '''Input: This Matrix'''
+    '''Output: Transpose Matrix'''
+    def transpose(self):
+        return Matrix([[row[i] for row in self.A] for i in range(self.rows)])
+
+    '''Guassian Elimination, solves System of Linear Equations'''
+    '''Input: This Matrix'''
+    '''Output: Reduced Row Echelon Form Matrix'''
+    def gauss_elimination(self):
+        # 1. Swap the rows so that all the rows with all 0 entries are on the bottom
+        last_zero = self.rows - 1
+        zero_test = [0 for i in range(self.cols)] # Create 0 row to compare other rows to
+        for row in range(self.rows - 1,0,-1):
+            if self.A[row] == zero_test and (last_zero < self.rows - 1):
+                self.swap(row, last_zero) 
+                last_zero -= 1 
+            elif self.A[row] == zero_test and (last_zero >= self.rows - 1): 
+                last_zero -= 1
+
+        # 5. Repeat steps 2-4 for the next leftmost nonzero entry until all the leading entries are 1.
+        for pivot_row in range(self.cols-1): # Go from x0 to xn, but do not include b, which is the last column ; Note Pivot_row == Pivot_column
+
+            # 2. Swap the rows so that the row with the largest, leftmost nonzero entry is on top.
+            nth_column = [self.A[i][pivot_row] for i in range(last_zero)] # Put pivot_column in place of 0
+            self.swap(nth_column.index(max(nth_column)), 0)
+            
+            # 3. Multiply the top row by a scalar so that top row's leading entry becomes 1.
+            self.row_multiply(pivot_row,1/self.A[pivot_row][pivot_row]) # Put pivot_row in place of 0, [pivot_row][pivot_row]
+
+            # 4. Add/subtract multiples of the top row to the other rows so that all other entries in the column containing the top row's leading entry are all zero.
+            for row in list(range(pivot_row)) + list(range(pivot_row+1,last_zero+1)): 
+                self.row_sum(pivot_row,row,multiple1=-self.A[row][pivot_row]) # Put pivot_column in place of 0, -row[0] ; Note: multiple1 will be equal to -rows_to_eliminate[row][pivot_column] because the number at pivot is always 1
+        
+        # 6. Swap the rows so that the leading entry of each nonzero row is to the right of the leading entry of the row above it.
+        for column in range(self.cols-1): # Go from x0 to xn, but do not include b, which is the last column
+            temp = [self.A[i][column] for i in range(last_zero+1)]
+            self.swap(temp.index(1), column)
+            
+        return self
+
+    '''Overload Addition operator to define Addition operation for Matrix Objects'''
+    '''Input: Matrix , Matrix or Matrix , Scalar'''
+    '''Output: Matrix'''
+    def __add__(self, other):
+        # Create 0 matrix that is the size of the input matrix
+        C = [[0 for j in range(self.cols)] for i in range(self.rows)]
+
+        # Ensure other object is a Matrix
+        if isinstance(other, Matrix):
+            # Add each element of the Matrices together
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    C[i][j] = self.A[i][j] + other.A[i][j]
+
+        elif isinstance(other, (int, float)):
+            # Add the Matrix with the scalar
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    C[i][j] = self.A[i][j] + other
+
+        # Note if other is neither a Matrix or a Scalar, this will return the 0 Matrix the size of the input Matrix
+        return Matrix(C)
+
+    '''Overload Left Addition operator to define Addition operation for Matrix Objects'''
+    '''Input: Matrix , Matrix or Matrix , Scalar'''
+    '''Output: Matrix'''
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    '''Overload Subtraction operator to define Subtraction operation for Matrix Objects'''
+    '''Input: Matrix , Matrix or Matrix , Scalar'''
+    '''Output: Matrix'''
+    def __sub__(self, other):
+        # Create 0 matrix that is the size of the input matrix
+        C = [[0 for j in range(self.cols)] for i in range(self.rows)]
+
+        # Ensure other object is a Matrix
+        if isinstance(other, Matrix):
+            # Subtract each element of the Matrices together
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    C[i][j] = self.A[i][j] - other.A[i][j]
+
+        elif isinstance(other, (int, float)):
+            # Subtract the Matrix with the scalar
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    C[i][j] = self.A[i][j] - other
+
+        # Note if other is neither a Matrix or a Scalar, this will return the 0 Matrix the size of the input Matrix
+        return Matrix(C)
+
+    '''Overload Left Subtraction operator to define Subtraction operation for Matrix Objects'''
+    '''Input: Matrix , Matrix or Matrix , Scalar'''
+    '''Output: Matrix'''
+    def __rsub__(self, other):
+        return self.__sub__(other)
+
+    '''Overload Multiplication operator to define Multiplication operation for Matrix Objects'''
+    '''Input: Matrix , Scalar or Scalar , Matrix'''
+    '''Output: Matrix'''
+    def __mul__(self, other):
+
+        C = [[0 for j in range(self.cols)] for i in range(self.rows)]
+
+        if isinstance(other, Matrix):
+
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    C[i][j] = self.A[i][j] * other.A[i][j]
+
+        # Scaler multiplication
+        elif isinstance(other, (int, float)):
+
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    C[i][j] = self.A[i][j] * other
+
+        return Matrix(C)
+
+    '''Overload Left Multiplication operator to define Multiplication operation for Matrix Objects'''
+    '''Input: Matrix , Scalar or Scalar , Matrix'''
+    '''Output: Matrix'''
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    '''Overload Division operator to define Division operation for Matrix Objects'''
+    '''Input: Matrix , Scalar or Scalar , Matrix'''
+    '''Output: Matrix'''
+    def __truediv__(self, other):
+
+        C = [[0 for j in range(self.cols)] for i in range(self.rows)]
+
+        if isinstance(other, Matrix):
+
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    C[i][j] = self.A[i][j] / other.A[i][j]
+
+        # Scaler multiplication
+        elif isinstance(other, (int, float)):
+
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    C[i][j] = self.A[i][j] / other
+
+        return Matrix(C)
+
+    '''Overload Left Division operator to define Division operation for Matrix Objects'''
+    '''Input: Matrix , Scalar or Scalar , Matrix'''
+    '''Output: Matrix'''
+    def __rtruediv__(self, other):
+        return self.__div__(other)
+
+    '''Overload Matrix Multiplication operator to define Multiplication operation between Matrix, Matrix'''
+    '''Input: Matrix , Matrix'''
+    '''Output: Matrix'''
+    def __matmul__(self, other):
+        if isinstance(other, Matrix):
+            C = [[0 for j in range(self.cols)] for i in range(self.rows)]
+
+            # Multiply the elements in the same row of the first matrix
+            # to the elements in the same col of the second matrix
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    acc = 0
+
+                    for k in range(self.rows):
+                        acc += self.A[i][k] * other.A[k][j]
+
+                    C[i][j] = acc
+
+        return Matrix(C)
+
+    '''Get the item at the specified coordinates in a Matrix object'''
+    def __getitem__(self, key):
+        if isinstance(key, tuple):
+            i = key[0]
+            j = key[1]
+            return self.A[i][j]
+
+    '''Set the item at the specified coordinates in a Matrix object'''
+    def __setitem__(self, key, value):
+        if isinstance(key, tuple):
+            i = key[0]
+            j = key[1]
+            self.A[i][j] = value
+
+    '''Display the Matrix in the console'''
+    def __str__(self):
+        matrixString = ''
+        for i in range(self.rows):
+            matrixString += "  ".join(
+                map(lambda x: '{0:8.3f}'.format(x), self.A[i]))+'\n'
+        return matrixString
+
+
+test = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+test2 = Matrix([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
+test3 = Matrix([[3,2,-1,1],[2,-2,4,-2],[-1,.5,-1,0]])
+test4 = Matrix([[3,2,-1,1],[2,-2,4,-2],[-1,.5,-1,0]])
+
+print("")
+print("Test 1")
+print("test+test2")
+print("")
+print("Expected:")
+print("  10.000  10.000  10.000\n  10.000  10.000  10.000\n  10.000  10.000  10.000")
+print("Got:")
+print(test+test2)
+
+print("")
+print("Test 2")
+print("test@test2")
+print("")
+print("Expected:")
+print("  30.000  24.000  18.000\n  84.000  69.000  54.000\n  138.000  114.000  90.000")
+print("Got:")
+print(test@test2)
+
+print("")
+print("Test 3")
+print("test*2")
+print("")
+print("Expected:")
+print("  2.000  4.000  6.000\n  8.000  10.000  12.000\n  14.000  16.000  18.000")
+print("Got:")
+print(test*2)
+
+print("")
+print("Test 4")
+print("test2-test")
+print("")
+print("Expected:")
+print("  8.000  6.000  4.000\n  2.000  0.000  -2.000\n  -4.000  -6.000  -8.000")
+print("Got:")
+print(test2-test)
+
+print("")
+print("Test 5")
+print("test/2")
+print("")
+print("Expected:")
+print("  0.500  1.000  1.500\n  2.000  2.500  3.000\n  3.500  4.000  4.500")
+print("Got:")
+print(test/2)
+
+print("")
+print("Test 6")
+print("test[0,1]")
+print("")
+print("Expected:")
+print("2")
+print("Got:")
+print(test[0,1])
+
+print("")
+print("Test 7")
+print("test[0,1] = 30000")
+print("")
+print("Expected:")
+print("  1.000  30000.000  3.000\n  4.000  5.000  6.000\n  7.000  8.000  9.000")
+print("Got:")
+test[0,1] = 30000
+print(test)
+test[0,1] = 2
+
+print("")
+print("Test 8")
+print("test.transpose()")
+print("")
+print("Expected:")
+print("  1.000  4.000  7.000\n  2.000  5.000  8.000\n  3.000  6.000  9.000")
+print("Got:")
+print(test.transpose())
+
+print("")
+print("Test 9")
+print("test.swap(1,2)")
+print("")
+print("Expected:")
+print("  1.000  2.000  3.000\n  7.000  8.000  9.000\n  4.000  5.000  6.000")
+print("Got:")
+print(test.swap(1,2))
+test.swap(1,2)
+
+print("")
+print("Test 10")
+print("test.row_multiply(2,2)")
+print("")
+print("Expected:")
+print("  1.000  2.000  3.000\n  4.000  5.000  6.000\n  14.000  16.000  18.000")
+print("Got:")
+print(test.row_multiply(2,2))
+test.row_multiply(2,.5)
+
+print("")
+print("Test 11")
+print("test.row_sum(1,2)")
+print("")
+print("Expected:")
+print("  1.000  2.000  3.000\n  4.000  5.000  6.000\n  11.000  13.000  15.000")
+print("Got:")
+print(test.row_sum(1,2))
+
+print("")
+print("Test 12")
+print("test4.gauss_elimination()")
+print("")
+print("Expected:")
+#print("  1.000  0.000 0.000 1.000\n  0.000  1.000  0.000 -2.000\n  0.000  0.000  1.000 -2.000")
+print("some stuff on the white board")
+print("Got:")
+print(test4.gauss_elimination())
